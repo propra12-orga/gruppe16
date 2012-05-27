acb197fd485eaa7b6d43e3cf20dab26666033675
Fabian Pegel <fabian@fapeg.com>
2012-05-28 00:55:30 +0200

    files

---
+++ src/bomberman/Block.java
@@ -0,0 +1,19 @@
+package bomberman;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+public class Block extends JLabel {
+    
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public boolean walkable;
+    public boolean destroyable ;
+    public boolean fin;
+    
+    
+    public Block(ImageIcon imageicon){
+        super(imageicon);
+    }
+}
