7d3edc5e6d36ed2ce1c82bb7310780e06412d0ef
Fabian Pegel <fabian@fapeg.com>
2012-05-30 22:43:50 +0200

    bomberman stirbt jetzt in allen himmelsrichtungen

--- src/bomberman/Explosion.java
+++ src/bomberman/Explosion.java
@@ -20,33 +20,54 @@ class Explosion extends TimerTask {
 	}
 	if(lf.block[lf.bombex-1][lf.bombey].destroyable==true) { //bombe 1oben
 		lf.block[lf.bombex-1][lf.bombey].setIcon(lf.explo);
+		lf.exploaktiv[lf.bombex-1][lf.bombey]=true;
+		k.killplayer(lf,lf.bombex-1,lf.bombey);
+
 		lf.timer.schedule(new Loeschen(lf.bombex-1,lf.bombey,lf), 1000);  	
 	}
 	if(lf.bombex+2<lf.block.length && lf.block[lf.bombex+1][lf.bombey].destroyable==true && lf.block[lf.bombex+2][lf.bombey].destroyable==true) { //bombe 2unten
 		lf.block[lf.bombex+2][lf.bombey].setIcon(lf.explo);
+		lf.exploaktiv[lf.bombex+2][lf.bombey]=true;
+		k.killplayer(lf,lf.bombex+2,lf.bombey);
+
 		lf.timer.schedule(new Loeschen(lf.bombex+2,lf.bombey,lf), 1000);  	
 	}
 	if(lf.bombex-2<lf.block.length && lf.block[lf.bombex-1][lf.bombey].destroyable==true && lf.block[lf.bombex-2][lf.bombey].destroyable==true) { //bombe 2oben
 		lf.block[lf.bombex-2][lf.bombey].setIcon(lf.explo);
+		lf.exploaktiv[lf.bombex-2][lf.bombey]=true;
+		k.killplayer(lf,lf.bombex-2,lf.bombey);
+
 		lf.timer.schedule(new Loeschen(lf.bombex-2,lf.bombey,lf), 1000);  	
 	}
 	if(lf.block[lf.bombex][lf.bombey+1].destroyable==true) { //bombe 1rechts
 		lf.block[lf.bombex][lf.bombey+1].setIcon(lf.explo);
+		lf.exploaktiv[lf.bombex][lf.bombey+1]=true;
+		k.killplayer(lf,lf.bombex,lf.bombey+1);
+
 		lf.timer.schedule(new Loeschen(lf.bombex,lf.bombey+1,lf), 1000);  	
 
 	}
 	if(lf.block[lf.bombex][lf.bombey-1].destroyable==true) { //bombe 1links
 		lf.block[lf.bombex][lf.bombey-1].setIcon(lf.explo);
+		lf.exploaktiv[lf.bombex][lf.bombey-1]=true;
+		k.killplayer(lf,lf.bombex,lf.bombey-1);
+
 		lf.timer.schedule(new Loeschen(lf.bombex,lf.bombey-1,lf), 1000);  	
 
 	}
 	if(lf.bombey+2<lf.block.length && lf.block[lf.bombex][lf.bombey+1].destroyable==true && lf.block[lf.bombex][lf.bombey+2].destroyable==true) { //bombe 2rechts
 		lf.block[lf.bombex][lf.bombey+2].setIcon(lf.explo);
+		lf.exploaktiv[lf.bombex][lf.bombey+2]=true;
+		k.killplayer(lf,lf.bombex,lf.bombey+2);
+
 		lf.timer.schedule(new Loeschen(lf.bombex,lf.bombey+2,lf), 1000);  	
 
 	}
 	if(lf.bombey-2<lf.block.length && lf.block[lf.bombex][lf.bombey-1].destroyable==true && lf.block[lf.bombex][lf.bombey-2].destroyable==true) { //bombe 2links
 		lf.block[lf.bombex][lf.bombey-2].setIcon(lf.explo);
+		lf.exploaktiv[lf.bombex][lf.bombey-2]=true;
+		k.killplayer(lf,lf.bombex,lf.bombey-2);
+
 		lf.timer.schedule(new Loeschen(lf.bombex,lf.bombey-2,lf), 1000);  	
 
 	}
